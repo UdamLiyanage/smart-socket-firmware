ef823c8e636867d92d824ec45e0e847b833535c7
UdamLiyanage <udam1998@gmail.com>
2020-05-21 09:28:45 +0530

    WifiManager Dependencies and AP connect logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include <Arduino.h>
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#include <WebServer.h>
+#else
+#error Invalid platform
+#endif
+#include <DNSServer.h>
+#include <WiFiManager.h>
 
 void setup() {
-  pinMode(12, OUTPUT);
+    Serial.begin(115200);
+    pinMode(12, OUTPUT);
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("AutoConnectAP");
+    Serial.println("connected...yeey :)");
 }
 
 void loop() {
   digitalWrite(12, HIGH);
-  delay(1000);
-  digitalWrite(12, LOW);
-  delay(1000);
+  delay(100);
+  digitalWrite(12,LOW);
+  delay(100);
 }
